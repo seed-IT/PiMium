2c874d2478542b1a70200efad988b86b840ba027
Melvin Campos Casares <melvin.campos@outlook.be>
2019-10-23 16:13:28 +0200

    Add timestamp and clean terminal in bme280.py

--- bme280/bme280.py
+++ bme280/bme280.py
@@ -3,12 +3,14 @@ import digitalio
 import board
 import busio
 import adafruit_bme280
+import os
+import datetime
 
-# Create library object using our Bus I2C port
+# Create library object using Bus I2C port
 #i2c = busio.I2C(board.SCL, board.SDA)
 #bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
-# OR create library object using our Bus SPI port
+# OR create library object using Bus SPI port
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 bme_cs = digitalio.DigitalInOut(board.D5)
 bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
@@ -21,11 +23,20 @@ bme280.iir_filter = adafruit_bme280.IIR_FILTER_X16
 bme280.overscan_pressure = adafruit_bme280.OVERSCAN_X16
 bme280.overscan_humidity = adafruit_bme280.OVERSCAN_X1
 bme280.overscan_temperature = adafruit_bme280.OVERSCAN_X2
+
+# Clear terminal on Windows and UNIX system before starting everything
+os.system('cls' if os.name == 'nt' else 'clear')
+print("Please note Altitude is calculated based on pressure information.")
+print("Starting record of BME280 sensor...")
+print("===================================")
+
 # The sensor will need a moment to gather initial readings
 time.sleep(1)
 
 while True:
-        print("\nTemperature: %0.1f C" % bme280.temperature)
+        now = datetime.datetime.now() # Get current date and time
+        print("\n" + now.strftime("%Y-%m-%d %H:%M:%S"))
+        print("Temperature: %0.1f C" % bme280.temperature)
         print("Humidity: %0.1f %%" % bme280.humidity)
         print("Pressure: %0.1f hPa" % bme280.pressure)
         print("Altitude = %0.2f meters" % bme280.altitude)
