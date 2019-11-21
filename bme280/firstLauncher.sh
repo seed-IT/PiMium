34735b8fa07bee3d1e6ba19a1d25ce42cf0047b2
Melvin Campos Casares <melvin.campos@outlook.be>
2019-11-21 01:18:53 +0100

    Add necessary modules in firstLauncher.sh
    RPI.GPIO, adafruit-blinka, adafruit-bme280, adafruit-circuitpython-bme280

--- bme280/firstLauncher.sh
+++ bme280/firstLauncher.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-#This one will automatically clone the repo with all the codefor the RaspBerry
+# This one will automatically clone the repo with all the code for the RaspBerry
+# It will also install the necessary modules
 
+sudo apt update -y
+sudo apt upgrade -y
+sudo pip install RPI.GPIO adafruit-blinka adafruit-bme280 adafruit-circuitpython-bme280
+sudo pip3 install RPI.GPIO adafruit-blinka adafruit-bme280 adafruit-circuitpython-bme280
+sudo pip install --upgrade setuptools
 cd /
 git clode https://github.com/seed-IT/PiMium.git rose
+
