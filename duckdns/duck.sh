0dc9d412a9e8a67f07c8e49d3b15fb612460a9f9
Melvin Campos Casares <melvin.campos@outlook.be>
2019-12-05 00:28:41 +0100

    Update duck.sh

--- duckdns/duck.sh
+++ duckdns/duck.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-echo url="https://www.duckdns.org/update?domains=seed-it&token=e7270911-1842-4813-92fd-2c8f693e86ec&verbose=true&pi=&ipv6=" | curl -k -o ~/Git/PiMium/duckdns/duck.log -K -
-
+if [ $# -gt 1 ]
+then
+        echo "Check arguments..."
+        echo "Usage: sudo ./duck.sh h3r3-1s-duckdns-t0k3n"
+else
+        echo "Given token is: $1"
+        echo url="https://www.duckdns.org/update?domains=seed-it&token=$1&verbose=true&pi=&ipv6=" | curl -k -o ~/Git/PiMium/duckdns/duck.log -K -
+fi
